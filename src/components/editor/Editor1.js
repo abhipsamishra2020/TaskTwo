d595572c9024cce89e2dc6e6f2d733da7a94bc7c
abhipsamishra2020 <abhipsapmishra@gmail.com>
2022-01-16 17:01:43 +0530

    complit task two

---
+++ src/components/editor/Editor1.js
@@ -0,0 +1,29 @@
+import Split from 'react-split';
+
+
+export const Editor1 = () =>{
+    return (
+        <div className='main-div'>
+       <Split direction='vertical'  style={{height:'calc(100vh - 4rem)'}}>
+            {/* <div className='bg-gray-400'></div> */}
+            <Split className='flex'>
+            <div className='bg-gray-400'>
+                <p className='change'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus odit libero impedit quisquam laudantium atque, neque pariatur nihil esse possimus perspiciatis, commodi voluptatem cupiditate, iste provident! Exercitationem, error! Dolorum animi adipisci quas quo?🎲
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quo, rerum doloribus temporibus quisquam facere! Animi quidem fugit pariatur necessitatibus totam!🛤️ Fugit, culpa eligendi ut et esse atque magnam quod maxime minus provident?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quas itaque officia eum explicabo provident iure quae, voluptatum aspernatur. Placeat, debitis consequuntur!🌍</p>
+            </div>
+            <div className='bg-gray-400'>
+                <p className='change'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, cum laborum aspernatur dolores non quasi. Sint, voluptatem. Quia in voluptas, deleniti tempora ea exercitationem deserunt beatae, vitae, repellat voluptates ad ipsa maiores ex autem ipsum. Ullam cum ab unde alias a fugit nisi aut nam totam voluptates saepe itaque debitis, sint illum error incidunt accusantium voluptas? Illo ab quae sunt vero aut et dolorem,✴️ pariatur, dolore voluptatem doloremque, expedit Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repellendus non corporis totam cum laudantium, porro expedita provident. Quia reprehenderit optio vel perspiciatis culpa,🤽 voluptate corporis ad ratione alias expedita est obcaecati debitis natus nisi deleniti quaerat corrupti sequi, voluptatibus iusto animi quae esse. Hic modi quod aut ipsum fugit mollitia nobis vitae architecto veritatis? Obcaecati rerum facere aliquam ipsam, repudiandae recusandae facilis asperiores possimus? Voluptate fugiat unde labore veritatis tempore impedit laborum, repudiandae eaque voluptatem. Eligendi expedita quidem accusantium.🏴󠁧󠁢󠁷󠁬󠁳󠁿
+                </p>
+            </div>
+            </Split>
+            <div className='bg-gray-500'>
+               
+            </div>
+        </Split>
+        </div>
+        
+    );
+};
